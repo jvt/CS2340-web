0f7c28084cd794a3205683911a1b6fd0d7cf37a6
Joe Torraca <joe@joetorraca.com>
2017-02-27 19:43:23 -0500

    Adds routes for getting user data and for saving new data

--- routes.js
+++ routes.js
@@ -4,7 +4,9 @@ var csrf = require('csurf');
 module.exports = function(app, c) {
 	// API routes
 	app.post('/api/session', c.api.createSession);
+	app.get('/api/user/:id', c.api.getUser);
 	app.post('/api/user', c.api.createUser);
+	app.post('/api/update', c.api.updateUser);
 
 	app.use(csrf());
 
