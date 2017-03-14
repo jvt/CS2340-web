a7a01851eec092a18b3eee5ffc6b029dff4c3bba
Joe Torraca <joe@joetorraca.com>
2017-03-14 13:28:37 -0400

    Register partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nconf = require('nconf');
+var hbs = require('hbs');
 var bookshelf = require('bookshelf');
 global.config = nconf.argv().env().file({ file: path.join(__dirname, 'config.json') });
 
@@ -20,6 +21,7 @@ bookshelf.DB = require('bookshelf')(knex);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
