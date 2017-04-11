cccb0c04c50f9601ee5c7394832722d9e05ace63
Joe Torraca <joe@joetorraca.com>
2017-04-10 22:25:35 -0400

    decimal->int

--- migrations/20170410221657_add_virusPPM_to_quality_table.js
+++ migrations/20170410221657_add_virusPPM_to_quality_table.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('quality', function (table) {
-    table.decimal('virusPPM', 5, 3).after('condition');
+    table.integer('virusPPM').after('condition');
   });
 };
 
--- migrations/20170410221705_add_contaminantPPM_to_quality_table.js
+++ migrations/20170410221705_add_contaminantPPM_to_quality_table.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('quality', function (table) {
-    table.decimal('contaminantPPM', 5, 3).after('virusPPM');
+    table.integer('contaminantPPM').after('virusPPM');
   });
 };
 
