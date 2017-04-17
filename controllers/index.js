cd7cfd681498cdc13f1183c5c10ed1a59b4a9c49
Joe Torraca <joe@joetorraca.com>
2017-04-17 10:38:07 -0400

    Make admin controller public

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
 exports.index = require('./home');
 exports.api = require('./api');
 exports.session = require('./session');
+exports.reports = require('./reports');
+exports.admin = require('./admin');
